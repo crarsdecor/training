968f4f5693d7232d47b03b50774769acbe07d895
FaizAhmad0 <118246870+FaizAhmad0@users.noreply.github.com>
2025-01-02 12:07:26 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const adminRoute = require("./route/adminRoute");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8800;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
